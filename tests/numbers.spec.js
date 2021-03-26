2b0cf3571b60faf2b3bb90a015113e97b49cf43a
Gildeir <gildeirlopes@gmail.com>
2021-03-26 18:00:56 -0300

    Corrgindo pontuaçao Requisito numbers

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -24,10 +24,10 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     // Escreva um teste em que a função recebe [' '] e retorna false
 
-    assert.strictEqual(numbers([1, 2, 3, 4, 5], true);
-    assert.strictEqual(numbers([1, 2, '3', 4, 5], false);
-    assert.strictEqual(numbers([1, 'a', 3], false);
-    assert.strictEqual(numbers([[' '], false);
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
+    assert.strictEqual(numbers([1, 'a', 3]), false);
+    assert.strictEqual(numbers([[' ']), false);
 
     
 
