1c2e812f4c265220b3fb54db54244be4b8b20272
Gildeir <gildeirlopes@gmail.com>
2021-03-27 17:38:45 -0300

    Corrigindo tamanho de retorno do objeto

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,7 +30,7 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(Object.keys(circle(3)).length);
+    assert.strictEqual(Object.keys(circle(3)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
