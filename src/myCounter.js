1e2855c44c12c2a83667873d005c72690d52c742
Gildeir <gildeirlopes@gmail.com>
2021-03-29 00:01:53 -0300

    Requisito myCounter, alterei tipo de variavel e renomeei a variavel count

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,10 +12,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
