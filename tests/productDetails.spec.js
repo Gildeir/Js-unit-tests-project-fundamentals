e04d0eba27dc278b89cd9c94021d0a5abe588bff
Gildeir <gildeirlopes@gmail.com>
2021-03-30 02:26:12 -0300

    Adicionando requisito Product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const productDts = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(Array.isArray(productDts), true);
     // Teste que o retorno da função é um array.
+    assert.strictEqual(productDts.length, 2);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.StrictEqual(typeof (productDts[0]), 'object');
+    assert.StrictEqual(typeof (productDts[1]), 'object');
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDts[0], productDts[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDts[0].endsWith(123));
+    assert.strictEqual(productDts[0].endsWith(123));
   });
 });
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,10 +52,11 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui,
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
-    // ```
+    
+    assert.strictEqual(typeof (createMenu.fetchMenu), 'object');
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`,
